c5fbf7e9e6bf8aff535435dbb8da91a0d2293ae3
Andrew Johnson <ajohnson.uva@gmail.com>
2023-12-06 09:07:45 -0500

    remove vite config for proxying requests

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy: {
-    //   '/query': {
-    //     target: 'https://data.flur.ee/fluree',
-    //     changeOrigin: true,
-    //   },
-    //   '/transact': {
-    //     target: 'https://data.flur.ee/fluree',
-    //     changeOrigin: true,
-    //   },
-    // },
-  },
+  server: {},
 });
